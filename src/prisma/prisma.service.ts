e85d3804c758548c3d0fc79b37fc27343db27e5d
Solon97 <solonb20@gmail.com>
2026-05-22 18:02:18 -0300

    feat(prisma): initialize Prisma setup with user and question models

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from './generated/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL!,
+      }),
+    });
+  }
+  onModuleInit() {
+    return this.$connect();
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
